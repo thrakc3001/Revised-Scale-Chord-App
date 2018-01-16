a67aa866afd6bb17b8738e9ebd5ba4586e48d155
thrakc3001 <cainmusicteach@gmail.com>
2018-01-15 21:48:55 -0600

    all buttons rendered

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      scales: [],
+      chords: [],
+      scaleKeys: [],
+      chordKeys: []
     };
   }
 
@@ -25,27 +28,104 @@ class App extends Component {
   getData() {
     firebase.database().ref().child('scales').on('value', snapshot => {
       this.setState({
-        data: snapshot.val()
+        scales: snapshot.val()
       })
     });
+
+    firebase.database().ref().child('chords').on('value', snapshot => {
+      this.setState({
+        chords: snapshot.val()
+      })
+    });
+
+    firebase.database().ref().child('scalekeys').on('value', snapshot => {
+      this.setState({
+        scaleKeys: snapshot.val()
+      })
+    });
+
+    firebase.database().ref().child('chordkeys').on('value', snapshot => {
+      this.setState({
+        chordKeys: snapshot.val()
+      })
+    });
+  }
+
+  handleClick() {
+
   }
 
 
   render() {
 
-    let btnStyles = {
+    let scaleStyles = {
       width: '90px',
       height: '15px',
-      fontSize: '8px'
+      fontSize: '8px',
+      display: 'none'
     }
 
-    let btn = this.state.data.map((result, i) => (
-      <button key={i} style={btnStyles}>{result}</button>
+    let chordStyles = {
+      width: '90px',
+      height: '15px',
+      fontSize: '8px',
+      display: 'none'
+    }
+
+    let keyStyles = {
+      width: '115px',
+      height: '30px',
+      fontSize: '8px',
+      display: 'flex',
+      justifyContent: 'center',
+      margin: '0 auto'
+    }
+
+    let keyContainer = {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      width: '32%',
+      justifyContent: 'center',
+      margin: '0 auto',
+      height: '128px',
+      paddingBottom: '5px'
+    }
+
+    let btnContainer = {
+    }
+
+    let scaleBtn = this.state.scales.map((result, i) => (
+      <button key={i} style={scaleStyles}>{result}</button>
+    ))
+
+    let chordBtn = this.state.chords.map((result, i) => (
+      <button key={i} style={chordStyles}>{result}</button>
+    ))
+
+    let scaleKeysBtn = this.state.scaleKeys.map((result, i) => (
+      <button key={i} style={keyStyles}>{result}</button>
+    ))
+
+    let chordKeysBtn = this.state.chordKeys.map((result, i) => (
+      // Make button trigger a mp3 from DB
+      // Make button trigger a png from DB
+      <button key={i} style={keyStyles} onClick={ () => {this.handleClick()} }>{result}</button>
     ))
     
     return (
       <div>
-        {btn}
+        <h1 style={{textAlign: 'center'}}>Scales and Chords</h1>
+        <div style={btnContainer}>
+          <div style={keyContainer}>
+          {scaleKeysBtn}
+          </div>
+          <div style={keyContainer}>
+          {chordKeysBtn}
+          </div>
+          {scaleBtn}
+          {chordBtn}
+        </div>
       </div>
     );
   }
